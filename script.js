21f27487014996e2a64958ca0acddc637191b277
Ephrim Bazile <89447659+kevenbazile@users.noreply.github.com>
2025-01-03 02:59:08 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,238 +1,176 @@
-(function() {
-    "use strict";
+const padBank1 = [
+    {
+        clipId: 'Heater-1',
+        clip: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3',
+        key: 'Q'
+    },
+    {
+        clipId: 'Heater-2',
+        clip: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3',
+        key: 'W'
+    },
+    {
+        clipId: 'Heater-3',
+        clip: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3',
+        key: 'E'
+    },
+    {
+        clipId: 'Heater-4',
+        clip: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4.mp3',
+        key: 'A'
+    },
+    {
+        clipId: 'Clap',
+        clip: 'https://s3.amazonaws.com/freecodecamp/drums/Clap.mp3',
+        key: 'S'
+    },
+    {
+        clipId: 'Open-HH',
+        clip: 'https://s3.amazonaws.com/freecodecamp/drums/Open-HH.mp3',
+        key: 'D'
+    },
+    {
+        clipId: 'Kick-n-Hat',
+        clip: 'https://s3.amazonaws.com/freecodecamp/drums/Kick-n-Hat.mp3',
+        key: 'Z'
+    },
+    {
+        clipId: 'Kick',
+        clip: 'https://s3.amazonaws.com/freecodecamp/drums/Kick.mp3',
+        key: 'X'
+    },
+    {
+        clipId: 'Closed-HH',
+        clip: 'https://s3.amazonaws.com/freecodecamp/drums/Closed-HH.mp3',
+        key: 'C'
+    }
+];
 
-    const heaterKit = [
-        { keyCode: 81, keyTrigger: "Q", id: "Heater-1", url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3" },
-        { keyCode: 87, keyTrigger: "W", id: "Heater-2", url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3" },
-        { keyCode: 69, keyTrigger: "E", id: "Heater-3", url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3" },
-        { keyCode: 65, keyTrigger: "A", id: "Heater-4", url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3" },
-        { keyCode: 83, keyTrigger: "S", id: "Clap", url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3" },
-        { keyCode: 68, keyTrigger: "D", id: "Open-HH", url: "https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3" },
-        { keyCode: 90, keyTrigger: "Z", id: "Kick-n'-Hat", url: "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3" },
-        { keyCode: 88, keyTrigger: "X", id: "Kick", url: "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3" },
-        { keyCode: 67, keyTrigger: "C", id: "Closed-HH", url: "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3" }
-    ];
+const padBank2 = [
+    {
+        clipId: 'Chord-1',
+        clip: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_1.mp3',
+        key: 'Q'
+    },
+    {
+        clipId: 'Chord-2',
+        clip: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_2.mp3',
+        key: 'W'
+    },
+    {
+        clipId: 'Chord-3',
+        clip: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_3.mp3',
+        key: 'E'
+    },
+    {
+        clipId: 'Shaker',
+        clip: 'https://s3.amazonaws.com/freecodecamp/drums/Give_us_a_light.mp3',
+        key: 'A'
+    },
+    {
+        clipId: 'Open-HH',
+        clip: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3',
+        key: 'S'
+    },
+    {
+        clipId: 'Punchy-Kick',
+        clip: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3',
+        key: 'D'
+    },
+    {
+        clipId: 'Brk-1',
+        clip: 'https://s3.amazonaws.com/freecodecamp/drums/Brk_1.mp3',
+        key: 'Z'
+    },
+    {
+        clipId: 'Brk-2',
+        clip: 'https://s3.amazonaws.com/freecodecamp/drums/Brk_2.mp3',
+        key: 'X'
+    },
+    {
+        clipId: 'Brk-3',
+        clip: 'https://s3.amazonaws.com/freecodecamp/drums/Brk_3.mp3',
+        key: 'C'
+    }
+];
 
-    const smoothPianoKit = [
-        { keyCode: 81, keyTrigger: "Q", id: "Chord-1", url: "https://s3.amazonaws.com/freecodecamp/drums/Chord_1.mp3" },
-        { keyCode: 87, keyTrigger: "W", id: "Chord-2", url: "https://s3.amazonaws.com/freecodecamp/drums/Chord_2.mp3" },
-        { keyCode: 69, keyTrigger: "E", id: "Chord-3", url: "https://s3.amazonaws.com/freecodecamp/drums/Chord_3.mp3" },
-        { keyCode: 65, keyTrigger: "A", id: "Shaker", url: "https://s3.amazonaws.com/freecodecamp/drums/Give_us_a_light.mp3" },
-        { keyCode: 83, keyTrigger: "S", id: "Open-HH", url: "https://s3.amazonaws.com/freecodecamp/drums/Dry_Ohh.mp3" },
-        { keyCode: 68, keyTrigger: "D", id: "Closed-HH", url: "https://s3.amazonaws.com/freecodecamp/drums/Bld_H1.mp3" },
-        { keyCode: 90, keyTrigger: "Z", id: "Punchy-Kick", url: "https://s3.amazonaws.com/freecodecamp/drums/punchy_kick_1.mp3" },
-        { keyCode: 88, keyTrigger: "X", id: "Side-Stick", url: "https://s3.amazonaws.com/freecodecamp/drums/side_stick_1.mp3" },
-        { keyCode: 67, keyTrigger: "C", id: "Snare", url: "https://s3.amazonaws.com/freecodecamp/drums/Brk_Snr.mp3" }
-    ];
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            display: 'Press a Pad',
+            currentPadBank: padBank1,
+            power: true,
+            sliderVal: 0.5
+        };
+        this.handlePowerToggle = this.handlePowerToggle.bind(this);
+        this.handleBankToggle = this.handleBankToggle.bind(this);
+        this.handlePadClick = this.handlePadClick.bind(this);
+        this.handleSliderChange = this.handleSliderChange.bind(this);
+    }
 
-    const defaultPadStyle = {
-        backgroundColor: "grey",
-        marginTop: 10,
-        boxShadow: "3px 3px 5px black"
-    };
-
-    const activePadStyle = {
-        backgroundColor: "orange",
-        boxShadow: "0 3px orange",
-        height: 77,
-        marginTop: 13
-    };
-
-    class DrumPad extends React.Component {
-        constructor(props) {
-            super(props);
-            this.state = {
-                padStyle: defaultPadStyle
-            };
-            this.playSound = this.playSound.bind(this);
-            this.handleKeyPress = this.handleKeyPress.bind(this);
-            this.activatePad = this.activatePad.bind(this);
-        }
-
-        componentDidMount() {
-            document.addEventListener("keydown", this.handleKeyPress);
-        }
-
-        componentWillUnmount() {
-            document.removeEventListener("keydown", this.handleKeyPress);
-        }
-
-        handleKeyPress(event) {
-            if (event.keyCode === this.props.keyCode) {
-                this.playSound();
-            }
-        }
-
-        activatePad() {
-            if (this.props.power) {
-                if (this.state.padStyle.backgroundColor === "orange") {
-                    this.setState({ padStyle: defaultPadStyle });
-                } else {
-                    this.setState({ padStyle: activePadStyle });
-                }
-            } else {
-                if (this.state.padStyle.marginTop === 13) {
-                    this.setState({ padStyle: defaultPadStyle });
-                } else {
-                    this.setState({ padStyle: { height: 77, marginTop: 13, backgroundColor: "grey", boxShadow: "0 3px grey" } });
-                }
-            }
-        }
-
-        playSound() {
-            const audio = document.getElementById(this.props.keyTrigger);
-            audio.currentTime = 0;
+    handlePadClick(clipId, clipUrl) {
+        if (this.state.power) {
+            const audio = new Audio(clipUrl);
+            audio.volume = this.state.sliderVal;
             audio.play();
-            this.activatePad();
-            setTimeout(() => this.activatePad(), 100);
-            this.props.updateDisplay(this.props.clipId.replace(/-/g, " "));
-        }
-
-        render() {
-            return React.createElement(
-                "div",
-                { className: "drum-pad", id: this.props.clipId, onClick: this.playSound, style: this.state.padStyle },
-                React.createElement("audio", { className: "clip", id: this.props.keyTrigger, src: this.props.clip }),
-                this.props.keyTrigger
-            );
+            this.setState({ display: clipId });
         }
     }
 
-    class PadBank extends React.Component {
-        constructor(props) {
-            super(props);
-        }
-
-        render() {
-            let padBank;
-            if (this.props.power) {
-                padBank = this.props.currentPadBank.map((pad, index, bank) =>
-                    React.createElement(DrumPad, {
-                        clip: bank[index].url,
-                        clipId: bank[index].id,
-                        key: bank[index].id,
-                        keyCode: bank[index].keyCode,
-                        keyTrigger: bank[index].keyTrigger,
-                        power: this.props.power,
-                        updateDisplay: this.props.updateDisplay
-                    })
-                );
-            } else {
-                padBank = this.props.currentPadBank.map((pad, index, bank) =>
-                    React.createElement(DrumPad, {
-                        clip: "#",
-                        clipId: bank[index].id,
-                        key: bank[index].id,
-                        keyCode: bank[index].keyCode,
-                        keyTrigger: bank[index].keyTrigger,
-                        power: this.props.power,
-                        updateDisplay: this.props.updateDisplay
-                    })
-                );
-            }
-            return React.createElement("div", { className: "pad-bank" }, padBank);
-        }
+    handlePowerToggle() {
+        this.setState(prevState => ({
+            power: !prevState.power,
+            display: prevState.power ? 'Power OFF' : 'Power ON'
+        }));
     }
 
-    class App extends React.Component {
-        constructor(props) {
-            super(props);
-            this.state = {
-                power: true,
-                display: String.fromCharCode(160),
-                currentPadBank: heaterKit,
-                currentPadBankId: "Heater Kit",
-                sliderVal: 0.3
-            };
-            this.displayClipName = this.displayClipName.bind(this);
-            this.selectBank = this.selectBank.bind(this);
-            this.adjustVolume = this.adjustVolume.bind(this);
-            this.powerControl = this.powerControl.bind(this);
-            this.clearDisplay = this.clearDisplay.bind(this);
-        }
-
-        powerControl() {
-            this.setState({
-                power: !this.state.power,
-                display: String.fromCharCode(160)
-            });
-        }
-
-        selectBank() {
-            if (this.state.power) {
-                if (this.state.currentPadBankId === "Heater Kit") {
-                    this.setState({ currentPadBank: smoothPianoKit, display: "Smooth Piano Kit", currentPadBankId: "Smooth Piano Kit" });
-                } else {
-                    this.setState({ currentPadBank: heaterKit, display: "Heater Kit", currentPadBankId: "Heater Kit" });
-                }
-            }
-        }
-
-        displayClipName(clipName) {
-            if (this.state.power) {
-                this.setState({ display: clipName });
-            }
-        }
-
-        adjustVolume(event) {
-            if (this.state.power) {
-                this.setState({
-                    sliderVal: event.target.value,
-                    display: "Volume: " + Math.round(100 * event.target.value)
-                });
-                setTimeout(() => this.clearDisplay(), 1000);
-            }
-        }
-
-        clearDisplay() {
-            this.setState({ display: String.fromCharCode(160) });
-        }
-
-        render() {
-            const powerStyle = this.state.power ? { float: "right" } : { float: "left" };
-            const bankStyle = this.state.currentPadBank === heaterKit ? { float: "left" } : { float: "right" };
-
-            document.querySelectorAll(".clip").forEach((audioElement) => {
-                audioElement.volume = this.state.sliderVal;
-            });
-
-            return React.createElement(
-                "div",
-                { id: "drum-machine" },
-                React.createElement(
-                    "div",
-                    { id: "display" },
-                    React.createElement("h1", null, this.state.display)
-                ),
-                React.createElement(
-                    "div",
-                    { id: "drum-pads" },
-                    React.createElement(PadBank, {
-                        currentPadBank: this.state.currentPadBank,
-                        power: this.state.power,
-                        updateDisplay: this.displayClipName
-                    })
-                ),
-                React.createElement(
-                    "div",
-                    { id: "controls" },
-                    React.createElement("button", { className: "control-btn", id: "power", onClick: this.powerControl }, "Power"),
-                    React.createElement("button", { className: "control-btn", id: "bank", onClick: this.selectBank, style: bankStyle }, "Bank"),
-                    React.createElement("input", {
-                        className: "control-btn",
-                        id: "volume",
-                        type: "range",
-                        min: 0,
-                        max: 1,
-                        step: 0.01,
-                        value: this.state.sliderVal,
-                        onChange: this.adjustVolume
-                    }),
-                    React.createElement("label", { className: "control-btn", htmlFor: "volume" }, "Volume")
-                )
-            );
-        }
+    handleBankToggle() {
+        this.setState(prevState => ({
+            currentPadBank: prevState.currentPadBank === padBank1 ? padBank2 : padBank1,
+            display: 'Changed Bank'
+        }));
     }
 
-    ReactDOM.render(React.createElement(App), document.getElementById("root"));
-})();
+    handleSliderChange(event) {
+        this.setState({ sliderVal: event.target.value });
+    }
+
+    render() {
+        return (
+            <div id="drum-machine">
+                <div id="display">{this.state.display}</div>
+                <div id="pads">
+                    {this.state.currentPadBank.map(pad => (
+                        <button
+                            key={pad.key}
+                            className="drum-pad"
+                            id={pad.clipId}
+                            onClick={() => this.handlePadClick(pad.clipId, pad.clip)}
+                        >
+                            {pad.key}
+                        </button>
+                    ))}
+                </div>
+                <div id="controls">
+                    <button onClick={this.handlePowerToggle}>
+                        {this.state.power ? 'Power OFF' : 'Power ON'}
+                    </button>
+                    <button onClick={this.handleBankToggle}>
+                        Switch Bank
+                    </button>
+                    <input
+                        type="range"
+                        min="0"
+                        max="1"
+                        step="0.01"
+                        value={this.state.sliderVal}
+                        onChange={this.handleSliderChange}
+                    />
+                </div>
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
